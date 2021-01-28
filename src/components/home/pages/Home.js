d2a44dbb59c5277e0f9580aeb8794232f06ead11
Nathan Loveless <nathansl2003@yahoo.com>
2021-01-28 12:58:19 -0500

    Added shared layout and designed to be passed children to make DRY code.

--- src/components/home/pages/Home.js
+++ src/components/home/pages/Home.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+import LayoutHFCRS from '../../common/layoutComponents/LayoutHFCRS';
+
+// Implementation makes use the layout: LayoutHFCRS.js
 
 const Home = ({ LoadingComponent }) => {
   return (
     <div>
       <LoadingComponent message="Rendering map and fetching saved cities..." />
-      On home page!
+      <LayoutHFCRS
+        HeaderComponents={null}
+        FooterComponents={null}
+        ContentComponents={null}
+        SidebarComponents={null}
+      />
     </div>
   );
 };

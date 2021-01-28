df87dc6de1dc00aae63dd7d478ca618cf6dceb75
Erik Scow <erikscow@gmail.com>
2021-01-27 20:54:04 -0800

    Added file for context api. Also wrapped <App/> component with the context provider component

---
+++ src/state/contexts.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+export const Context = React.createContext();
+
+export const ContextProvider = props => {
+  //Any global state variables should be declared with useState here and added as a property to the state object, to then be passed in the value prop of the provider
+  const [userInfo, setUserInfo] = useState([]);
+
+  let state = {
+    userInfo: [userInfo, setUserInfo],
+  };
+
+  return <Context.Provider value={state}>{props.children}</Context.Provider>;
+};

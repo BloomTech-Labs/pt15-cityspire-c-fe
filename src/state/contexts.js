4853b21645c9c2cd9e519157255a8599b5061e4c
Erik Scow <erikscow@gmail.com>
2021-01-28 18:41:21 -0800

    Added savedCities to the context state object

--- src/state/contexts.js
+++ src/state/contexts.js
@@ -5,9 +5,11 @@ export const Context = React.createContext();
 export const ContextProvider = props => {
   //Any global state variables should be declared with useState here and added as a property to the state object, to then be passed in the value prop of the provider
   const [userInfo, setUserInfo] = useState([]);
+  const [savedCities, setSavedCities] = useState([]);
 
   let state = {
     userInfo: [userInfo, setUserInfo],
+    savedCities: [savedCities, setSavedCities],
   };
 
   return <Context.Provider value={state}>{props.children}</Context.Provider>;

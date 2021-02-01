44cd21489f53c0559ffcf500b73d8b45b7dce0f4
Elijah Atkins <elijahorama@gmail.com>
2021-01-31 18:40:05 -0700

    landing page start

--- src/components/landing/components/RenderLandingPage.js
+++ src/components/landing/components/RenderLandingPage.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import city1 from '../../assets/img/pexels-aleksejs-bergmanis-681335.jpg';
+import city2 from '../../assets/img/pexels-kehn-hermano-3849167.jpg';
+import city3 from '../../assets/img/pexels-tatiana-fet-1105766.jpg';
 
 function RenderLandingPage(props) {
   return (
     <div>
-      <h1>Welcome to Labs Basic SPA</h1>
+      <h2>Use data to find a place right for you to live.</h2>
       <div>
         <p>
-          This is an example of how we'd like for you to approach page/routable
-          components.
+          Looking for a fresh start for your family? Unsure where to go? Let
+          Cityspire help you find and manage all the data you need to decide.
+        </p>
+        <img src={city3}></img>
+        <p>
+          Cityspire is an app that analyzes data from cities such as
+          populations, cost of living, rental rates, crime rates, and many other
+          social and economic factors that are important in deciding where
+          someone would like to live.
         </p>
         <p>
-          <Link to="/example-list">Example List of Items</Link>
+          <Link to="/home">Home</Link>
         </p>
       </div>
     </div>
--- src/components/landing/pages/LandingContainer.js
+++ src/components/landing/pages/LandingContainer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import RenderLandingPage from '../components/RenderLandingPage';
+import LandingHeader from '../components/LandingHeader';
+import LayoutHFCRS from '../../common/layoutComponents/LayoutHFCRS';
 
 function LandingContainer({ LoadingComponent }) {
   return (
     <>
+      <LandingHeader />
       <RenderLandingPage />
     </>
   );

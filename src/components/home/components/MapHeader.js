9954205ec432c32f01162777c06e2e45347375fd
Nathan Loveless <nathansl2003@yahoo.com>
2021-02-02 20:08:48 -0500

    installed algolia-places-react, added component to header

--- src/components/home/components/MapHeader.js
+++ src/components/home/components/MapHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Avatar, Button, Input } from 'antd';
 import '../../../antD/styles/mapHeader.css';
+import AlgoliaPlaces from 'algolia-places-react';
 
 const MapHeader = () => {
   const { Search } = Input;
@@ -11,11 +12,39 @@ const MapHeader = () => {
   return (
     <div className="map-header">
       <div className="left-header-section">
-        <Search
-          placeholder="enter city,state"
-          onSearch={onSearch}
-          style={{ width: 300 }}
-          enterButton
+        <AlgoliaPlaces
+          placeholder="Search for a city"
+          options={{
+            appId: 'pl84ZNIL3EXV',
+            apiKey: '5e5fd2dfa544e1c1a95a53247ff59920',
+            language: 'en',
+            countries: ['us'],
+            type: 'city',
+          }}
+          onChange={({ query, rawAnswer, suggestion, suggestionIndex }) =>
+            console.log(
+              'Fired when suggestion selected in the dropdown or hint was validated.'
+            )
+          }
+          onSuggestions={({ rawAnswer, query, suggestions }) =>
+            console.log(
+              'Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed.'
+            )
+          }
+          onCursorChanged={({ rawAnswer, query, suggestion, suggestonIndex }) =>
+            console.log(
+              'Fired when arrows keys are used to navigate suggestions.'
+            )
+          }
+          onClear={() => console.log('Fired when the input is cleared.')}
+          onLimit={({ message }) =>
+            console.log('Fired when you reached your current rate limit.')
+          }
+          onError={({ message }) =>
+            console.log(
+              'Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit.'
+            )
+          }
         />
       </div>
       <div className="right-header-section">
@@ -29,3 +58,11 @@ const MapHeader = () => {
 };
 
 export default MapHeader;
+{
+  /* <Search
+placeholder="enter city,state"
+onSearch={onSearch}
+style={{ width: 300 }}
+enterButton
+/> */
+}

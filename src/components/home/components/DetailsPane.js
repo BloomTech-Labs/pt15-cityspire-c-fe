ecdce6f822be059959b3691bc4ea4edf24077768
Nathan Loveless <nathansl2003@yahoo.com>
2021-02-16 16:47:43 -0500

    Removed redundant code for Details Pane

--- src/components/home/components/DetailsPane.js
+++ src/components/home/components/DetailsPane.js
@@ -3,12 +3,6 @@ import React, { useState, useEffect } from 'react';
 import '../../../antD/styles/detailsPane.css';
 
 const DetailsPane = props => {
-  const [hidden, sethidden] = useState(false);
-
-  useEffect(() => {
-    sethidden(props.display);
-  }, [props.display]);
-
   return <div className="details-pane">This is the details pane</div>;
 };
 

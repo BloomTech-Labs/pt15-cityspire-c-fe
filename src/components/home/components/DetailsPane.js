8b5c3bd00b6b1cb1ef2e04ace5377c95bdd9b1e3
Erik Scow <erikscow@gmail.com>
2021-02-25 21:11:27 -0800

    Edits to city card data/props structure. Started api calls to finish favorites flow.

--- src/components/home/components/DetailsPane.js
+++ src/components/home/components/DetailsPane.js
@@ -1,13 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 
 import '../../../antD/styles/detailsPane.css';
+import Button from '../../common/Button';
 
 import axios from 'axios';
+import { Context } from '../../../state/contexts';
 
 import { axiosCodes, axiosAPICall } from '../../../utils/axiosEndpoints';
 
 const DetailsPane = ({ currentPlaceSelection }) => {
+  const { userInfo, savedCities } = useContext(Context);
+  const [userData, setUserData] = userInfo;
+  const [saved, setSaved] = savedCities;
+
   const [cityData, setCityData] = useState({});
+
+  const saveToState = () => {
+    setSaved([...saved, currentPlaceSelection]);
+  };
+
+  const saveToDb = () => {
+    axiosAPICall(
+      `/favorites/favoritesRouter/`,
+      axiosCodes.POST,
+      { ...userData.id, ...cityData },
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log('error: ', err);
+      }
+    );
+  };
+
+  const save = () => {
+    saveToState();
+    saveToDb();
+  };
 
   useEffect(() => {
     axios
@@ -29,6 +58,7 @@ const DetailsPane = ({ currentPlaceSelection }) => {
 
   return (
     <div className="details-pane">
+      <Button handleClick={save} buttonText="Save" />
       <p>Name: {cityData.city_name}</p>
       <p>Population: {cityData.population}</p>
       <p>Average Rent: {cityData.rent_per_month}</p>

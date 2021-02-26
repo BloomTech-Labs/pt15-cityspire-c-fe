06a2d13d053d3d519b3c020f2350bf4313092ebc
Erik Scow <erikscow@gmail.com>
2021-02-25 21:09:12 -0800

    removed console log in axioswithauth.js

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -4,7 +4,6 @@ export const axiosWithAuth = () => {
   const oktaTokenStorage = JSON.parse(
     localStorage.getItem('okta-token-storage')
   );
-  console.log(oktaTokenStorage);
   const token = 'Bearer ' + oktaTokenStorage.idToken.value;
 
   return axios.create({
